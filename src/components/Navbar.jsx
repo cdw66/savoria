9bf5f1fb6782e2e1783e4f29899e07b08f071ba1
Carter Wein <cwein66@gmail.com>
2023-09-12 21:46:44 -0400

    Added 404 page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,195 +33,190 @@ const Navbar = () => {
   const { classes } = useStyles();
 
   return (
-    <>
-      <nav className="flex justify-between items-center px-[24px] py-2 drop-shadow-sm">
-        <Link href="/">
-          <Image
-            width={130}
-            height={60}
-            src="/images/savoria_logo_transparent.png"
-            className={`object-contain cursor-pointer sm:hidden`}
-            alt="Savoria logo"
-          />
+    // <>
+    <nav className="flex justify-between items-center px-[24px] py-2 shadow-md">
+      <Link href="/">
+        <Image
+          width={130}
+          height={60}
+          src="/images/savoria_logo_transparent.png"
+          className={`object-contain cursor-pointer sm:hidden`}
+          alt="Savoria logo"
+        />
 
-          <Image
-            width={200}
-            height={100}
-            src="/images/savoria_logo_transparent.png"
-            className={`object-contain cursor-pointer hidden sm:inline-block`}
-            alt="Savoria logo"
-          />
+        <Image
+          width={200}
+          height={100}
+          src="/images/savoria_logo_transparent.png"
+          className={`object-contain cursor-pointer hidden sm:inline-block`}
+          alt="Savoria logo"
+        />
+      </Link>
+
+      <div className="lg:flex hidden gap-8 font-eb-garamond text-[18px]">
+        <Link
+          href="/"
+          className={`${
+            pathname === "/" && "text-tan"
+          } hover:text-tan duration-200`}
+        >
+          Home
+        </Link>
+        <Link
+          href="/menu"
+          className={`${
+            pathname === "/menu" && "text-tan "
+          } hover:text-tan duration-200`}
+        >
+          Menu
         </Link>
 
-        <div className="lg:flex hidden gap-8 font-eb-garamond text-[18px]">
-          <Link
-            href="/"
-            className={`${
-              pathname === "/" && "text-tan"
-            } hover:text-tan duration-200`}
-          >
-            Home
-          </Link>
-          <Link
-            href="/menu"
-            className={`${
-              pathname === "/menu" && "text-tan "
-            } hover:text-tan duration-200`}
-          >
-            Menu
-          </Link>
+        <Menu classNames={{ item: classes.item, dropdown: classes.dropdown }}>
+          <Menu.Target>
+            <span className="cursor-pointer">Locations & Hours</span>
+          </Menu.Target>
 
-          <Menu classNames={{ item: classes.item, dropdown: classes.dropdown }}>
-            <Menu.Target>
-              <span className="cursor-pointer">Locations & Hours</span>
-            </Menu.Target>
-
-            <Menu.Dropdown>
-              <Menu.Item>
-                <Link
-                  href="/locations/philadelphia"
-                  className={`${
-                    pathname === "/locations/philadelphia" && "text-tan"
-                  } hover:text-tan duration-200`}
-                >
-                  Philadelphia
-                </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link
-                  href="/locations/nyc"
-                  className={`${
-                    pathname === "/locations/nyc" && "text-tan"
-                  } hover:text-tan duration-200`}
-                >
-                  New York City
-                </Link>
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-
-          <Link
-            href="/reservations"
-            className={`${
-              pathname === "/reservations" && "text-tan"
-            } hover:text-tan duration-200`}
-          >
-            Reserve a Table
-          </Link>
-          <Link
-            href="/about"
-            className={`${
-              pathname === "/about" && "text-tan"
-            } hover:text-tan duration-200`}
-          >
-            Our Story
-          </Link>
-
-          {currentUser && !user.loadingUser && (
-            <Link
-              href="/auth/admin"
-              className={`${
-                pathname === "/auth/admin" && "text-tan"
-              } hover:text-tan duration-200`}
-            >
-              Admin
-            </Link>
-          )}
-        </div>
-
-        {/* Mobile Nav */}
-        <IconMenu2
-          onClick={open}
-          width={32}
-          height={32}
-          className="sm:hidden"
-        />
-
-        <IconMenu2
-          onClick={open}
-          width={48}
-          height={48}
-          className="lg:hidden sm:inline-block hidden"
-        />
-
-        <Drawer
-          opened={opened}
-          onClose={close}
-          overlayProps={{ opacity: 0.5, blur: 4 }}
-          position="right"
-          size={300}
-        >
-          {/* Drawer content */}
-          <div>
-            <NavLink
-              label="Home"
-              href="/"
-              component={Link}
-              onClick={close}
-              classNames={{ label: classes.label }}
-              className={`${pathname === "/" && "text-tan"}`}
-            />
-            <NavLink
-              label="Menu"
-              href="/menu"
-              component={Link}
-              onClick={close}
-              classNames={{ label: classes.label }}
-              className={`${pathname === "/menu" && "text-tan"}`}
-            />
-            <NavLink
-              label="Locations & Hours"
-              classNames={{ label: classes.label }}
-            >
-              <NavLink
-                label="Philadelphia"
+          <Menu.Dropdown>
+            <Menu.Item>
+              <Link
                 href="/locations/philadelphia"
-                component={Link}
-                onClick={close}
-                classNames={{ label: classes.label }}
                 className={`${
                   pathname === "/locations/philadelphia" && "text-tan"
-                }`}
-              />
-              <NavLink
-                label="New York City"
+                } hover:text-tan duration-200`}
+              >
+                Philadelphia
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link
                 href="/locations/nyc"
-                component={Link}
-                onClick={close}
-                classNames={{ label: classes.label }}
-                className={`${pathname === "/locations/nyc" && "text-tan"}`}
-              />
-            </NavLink>
+                className={`${
+                  pathname === "/locations/nyc" && "text-tan"
+                } hover:text-tan duration-200`}
+              >
+                New York City
+              </Link>
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+
+        <Link
+          href="/reservations"
+          className={`${
+            pathname === "/reservations" && "text-tan"
+          } hover:text-tan duration-200`}
+        >
+          Reserve a Table
+        </Link>
+        <Link
+          href="/about"
+          className={`${
+            pathname === "/about" && "text-tan"
+          } hover:text-tan duration-200`}
+        >
+          Our Story
+        </Link>
+
+        {currentUser && !user.loadingUser && (
+          <Link
+            href="/auth/admin"
+            className={`${
+              pathname === "/auth/admin" && "text-tan"
+            } hover:text-tan duration-200`}
+          >
+            Admin
+          </Link>
+        )}
+      </div>
+
+      {/* Mobile Nav */}
+      <IconMenu2 onClick={open} width={32} height={32} className="sm:hidden" />
+
+      <IconMenu2
+        onClick={open}
+        width={48}
+        height={48}
+        className="lg:hidden sm:inline-block hidden"
+      />
+
+      <Drawer
+        opened={opened}
+        onClose={close}
+        overlayProps={{ opacity: 0.5, blur: 4 }}
+        position="right"
+        size={300}
+      >
+        {/* Drawer content */}
+        <div>
+          <NavLink
+            label="Home"
+            href="/"
+            component={Link}
+            onClick={close}
+            classNames={{ label: classes.label }}
+            className={`${pathname === "/" && "text-tan"}`}
+          />
+          <NavLink
+            label="Menu"
+            href="/menu"
+            component={Link}
+            onClick={close}
+            classNames={{ label: classes.label }}
+            className={`${pathname === "/menu" && "text-tan"}`}
+          />
+          <NavLink
+            label="Locations & Hours"
+            classNames={{ label: classes.label }}
+          >
             <NavLink
-              label="Reserve a Table"
-              href="/reservations"
+              label="Philadelphia"
+              href="/locations/philadelphia"
               component={Link}
               onClick={close}
               classNames={{ label: classes.label }}
-              className={`${pathname === "/reservations" && "text-tan"}`}
+              className={`${
+                pathname === "/locations/philadelphia" && "text-tan"
+              }`}
             />
             <NavLink
-              label="Our Story"
-              href="/about"
+              label="New York City"
+              href="/locations/nyc"
               component={Link}
               onClick={close}
               classNames={{ label: classes.label }}
-              className={`${pathname === "/about" && "text-tan"}`}
+              className={`${pathname === "/locations/nyc" && "text-tan"}`}
             />
-            {currentUser && !user.loadingUser && (
-              <NavLink
-                label="Admin"
-                href="/auth/admin"
-                component={Link}
-                onClick={close}
-                classNames={{ label: classes.label }}
-                className={`${pathname === "/auth/admin" && "text-tan"}`}
-              />
-            )}
-          </div>
-        </Drawer>
-      </nav>
-    </>
+          </NavLink>
+          <NavLink
+            label="Reserve a Table"
+            href="/reservations"
+            component={Link}
+            onClick={close}
+            classNames={{ label: classes.label }}
+            className={`${pathname === "/reservations" && "text-tan"}`}
+          />
+          <NavLink
+            label="Our Story"
+            href="/about"
+            component={Link}
+            onClick={close}
+            classNames={{ label: classes.label }}
+            className={`${pathname === "/about" && "text-tan"}`}
+          />
+          {currentUser && !user.loadingUser && (
+            <NavLink
+              label="Admin"
+              href="/auth/admin"
+              component={Link}
+              onClick={close}
+              classNames={{ label: classes.label }}
+              className={`${pathname === "/auth/admin" && "text-tan"}`}
+            />
+          )}
+        </div>
+      </Drawer>
+    </nav>
+    // </>
   );
 };
 
---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Custom404 = () => {
+  return (
+    <div className="flex flex-col w-full h-[80vh] justify-center items-center">
+      <h1 className="font-eb-garamond text-[48px]">Page not Found</h1>
+      <p className="font-lato text-[18px] text-tan">
+        Sorry, the page you're looking for doesn't exist.
+      </p>
+    </div>
+  );
+};
+
+export default Custom404;
